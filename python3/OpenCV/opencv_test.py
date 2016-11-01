a66a1a9bef4a55784656a32b77a08abfca7bc0c0
ringo <blackapple262@gmail.com>
2016-11-01 18:23:47 +0900

    グレースケールできた

--- python3/OpenCV/opencv_test.py
+++ python3/OpenCV/opencv_test.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import cv2
+
 img = cv2.imread('lena.jpg')
-cv2.imshow('img',img)
+cv2.imshow('img', img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # grayscale conversion
+cv2.imshow("gray", gray)
 cv2.waitKey(0)
