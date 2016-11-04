d59c3513e31e800f57ec125bf200558c12679992
ringo <blackapple262@gmail.com>
2016-11-05 00:51:51 +0900

    課題終了

--- python3/OpenCV/faceDetect.py
+++ python3/OpenCV/faceDetect.py
@@ -4,30 +4,30 @@ import cv2
 import numpy as np
 import os
 
-# cascade_path = "haarcascades/haarcascade_frontalface_alt.xml"
-# faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-
-# cascade_path = os.path.dirname((os.path.abspath(__file__)) +
-#                               "/haarcascades/haarcascade_frontalface_alt.xml")
-cascade_path='C:/Users/ringo/Anaconda3/Library/etc/haarcascades/harcascade_frontalface_alt.xml'
+eye_path = 'C:/opencv-3.1.0/sources/data/haarcascades/haarcascade_eye.xml'
+cascade_path =  'C:/opencv-3.1.0/sources/data/haarcascades/haarcascade_frontalface_alt.xml'
 
 faceCascade = cv2.CascadeClassifier(cascade_path)
+eyeCascade = cv2.CascadeClassifier(eye_path)
+# img = cv2.imread('lena.jpg')
+img = cv2.imread('musicForever.png')
+cv2.imshow("origin", img)
 
-img = cv2.imread('lena.jpg')
-
-# cv2.imshow('img', img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # grayscale conversion
-# face = faceCascade.detectMultiScale(gray, 1.3, 5)
 face = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1,
                                     minSize=(1, 1))
-
-if len(face) > 0:
-    for rect in face:
-        cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0,
-                      0, 255), thickness=2)
-else:
-    print ('no face')
-
+for (x, y, w, h) in face:
+    # 検知した顔を矩形で囲む
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    # 顔画像（グレースケール）
+    roi_gray = gray[y:y+h, x:x+w]
+    # 顔ｇ増（カラースケール）
+    roi_color = img[y:y+h, x:x+w]
+    # 顔の中から目を検知
+    eyes = eyeCascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        # 検知した目を矩形で囲む
+        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
 cv2.imshow("detect", img)
-cv2.imshow("gray", gray)
+cv2.imwrite('result.jpg', img)
 cv2.waitKey(0)
