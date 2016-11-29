806971266c9c5fda1ff6182c2232a14a12bced70
ringo156 <blackapple262@gmail.com>
2016-11-29 23:09:39 +0900

    トリミングして出力

--- C/OpenCV/cascade_test.cpp
+++ C/OpenCV/cascade_test.cpp
@@ -3,10 +3,32 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 #include <iostream>
-
+#include <stdio.h>
+#include <string>
 
 using namespace cv;
 using namespace std;
+
+Mat detectFaceInImage(Mat &image, string &cascade_file);
+string intToString(int val);
+
+int main(int argc, char const *argv[])
+{
+    Mat image = imread("image-18.jpg");
+    string filename = "/home/ringo/prog/opencv-2.4.13/data/haarcascades/haarcascade_frontalface_alt.xml";
+    Mat detectFaceImage = detectFaceInImage(image, filename);
+    imshow("detect face", detectFaceImage);
+    waitKey(0);
+
+    return 0;
+}
+
+string intToString(int val)
+{
+  stringstream ss;
+  ss << val;
+  return ss.str();
+}
 
 Mat detectFaceInImage(Mat &image, string &cascade_file)
 {
@@ -15,28 +37,25 @@ Mat detectFaceInImage(Mat &image, string &cascade_file)
 
     vector<Rect> faces;
     cascade.detectMultiScale(image, faces, 1.1, 3, 0, Size(20, 20));
+    /*
     cout << "x=" << faces[0].x << endl;
     cout << "y=" << faces[0].y << endl;
     cout << "width=" << faces[0].width << endl;
     cout << "height=" << faces[0].height << endl;
-
-    Mat cut_img(image, cv::Rect(faces[0].x, faces[0].y, faces[0].width, faces[0].height));
+    */
+    //顔画像の切り抜き
+    for (int i=0; i < faces.size(); i++){
+      Mat cut_img(image, cv::Rect(faces[i].x, faces[i].y, faces[i].width, faces[i].height));
+      string str = intToString(i);//名前付ける
+      imshow(str, cut_img);
+      string jpg = ".jpg";
+      str = str + jpg;
+      imwrite (str, cut_img);
+    }
     for (int i = 0; i < faces.size(); i++) {
         rectangle(image, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar(0, 200, 0), 3, CV_AA);
     }
-    //Mat cut_img(image, cv::Rect(faces[0].x, faces[0].y, faces[0].x + faces[0].width, faces[0].y + faces[0].height));
-    //imwrite("cut.jpg", cut_img);
-    imshow("cut_face", cut_img);
+    imwrite("result.jpg", image);
+    //imshow("cut_face", cut_img);
     return image;
-}
-
-int main(int argc, char const *argv[])
-{
-    Mat image = imread("lena.jpg");
-    string filename = "/home/ringo/prog/opencv-2.4.13/data/haarcascades/haarcascade_frontalface_alt.xml";
-    Mat detectFaceImage = detectFaceInImage(image, filename);
-    imshow("detect face", detectFaceImage);
-    waitKey(0);
-
-    return 0;
 }
