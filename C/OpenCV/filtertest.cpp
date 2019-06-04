5323fc97ba2fee700379750437edafb53460705c
ringo <blackapple262@gmail.com>
2019-06-04 18:17:30 +0900

    add one dimension of median filter for pointer access

--- C/OpenCV/filtertest.cpp
+++ C/OpenCV/filtertest.cpp
@@ -6,7 +6,8 @@
 using namespace cv;
 using namespace std;
 
-void median_one(const Mat& src, Mat& dist, const int size);
+void median_one(const cv::Mat& src, cv::Mat& dist);
+void median_one_ptr(const cv::Mat& src, cv::Mat& dist);
 
 int main(int argc, const char** argv ) {
 
@@ -14,70 +15,24 @@ int main(int argc, const char** argv ) {
     Mat image = imread("lena.jpg");
     int width = image.cols;
     int height = image.rows;
-    //HSV
+
+    //HSV conversion---------
     Mat hsv_image;
     cvtColor(image, hsv_image, CV_BGR2HSV);
 
     Mat binary_image;
-    // extruction for HSV
+    // extruction for HSV---------
     inRange(hsv_image,
         Scalar(0, 0, 0, 0),
         Scalar(15, 255, 255, 0),
         binary_image
     );
 
-    Mat median_test = binary_image.clone();
-    //Mat median_test(binary_image.rows, binary_image.cols, CV_8UC1);
-    //uchar* ptr = median_test.ptr<uchar>(0);
-
-    // for(int j = 0; j < median_test.rows; j++){
-    //     for(int i = 0; i < median_test.cols; i++){
-    //         cout << (int)median_test.at<unsigned char>(j, i) << endl; // 255 is white
-    //     }
-    // }
-
-    int buf = 0;
-    for(int y = 0; y < median_test.rows;y++){
-        for(int x = 15; x < median_test.cols - 15; x++){
-            buf = 0;
-            for(int i = 0; i < 15; i++){
-                if((int)median_test.at<unsigned char>(y, (x + i - 7)) == 0){
-                    buf++;
-                }
-                if(buf == 9){
-                    median_test.at<unsigned char>(y, x - 7) = 0;
-                    break;
-                }
-                else{
-                    median_test.at<unsigned char>(y, x - 7) = 255;
-                }
-            }
-        }
-    }
-
-/*
-    for(int y = 0; y < median_test.rows * median_test.cols ; y = y + median_test.cols){
-        for(int x = 0; x < median_test.cols - 40; ++x){
-            for(int i = 0; i < 15; ++i){
-                int buf = 0;
-                // 画素値にアクセスして加算する
-                //Vec3b* ptr = median_test.ptr<Vec3b>(y); // バグる（笑）
-                //uchar* ptr = median_test.ptr<uchar>(y);
-                //buf += ptr[i + x];
-
-                buf += (int)median_test.at<unsigned char>(x + i, y);
-
-                if(buf >= 2295){
-                    //画素を黒にする
-                    median_test.at<unsigned char>(x) = 255;
-                    break;
-                }
-            }
-        }
-    }
-    */
-
-
+    Mat median_test;
+    // median filter of one dimension for pointer access
+    median_one_ptr(binary_image, median_test);
+    
+    // median conversion for OpenCV medianBlur
     Mat median_image;
     medianBlur(binary_image, median_image, 7);
 
@@ -104,6 +59,48 @@ int main(int argc, const char** argv ) {
     return 0;
 }
 
-void median_one(const Mat& src, Mat& dist, const int size){
 
+void median_one(const cv::Mat& src, cv::Mat& dist, const int size){
+    dist = src.clone();
+    int buf = 0;
+    for(int y = 0; y < src.rows;y++){
+        for(int x = 14; x < src.cols - 15; x++){
+            buf = 0;
+            for(int i = 0; i < 15; i++){
+                if((int)src.at<unsigned char>(y, (x + i - 7)) == 0){ // 0 is black
+                    buf++;
+                }
+                if(buf == 9){
+                    dist.at<unsigned char>(y, x - 7) = 0;
+                    break;
+                }
+                else{
+                    dist.at<unsigned char>(y, x - 7) = 255;
+                }
+            }
+        }
+    }
+}
+
+void median_one_ptr(const cv::Mat& src, cv::Mat& dist){
+    dist = src.clone();
+    uchar* ptr = dist.ptr<unsigned char>(0);
+    int buf = 0;
+    for(int y = 0; y < src.rows * src.cols; y = y + src.cols){
+        for(int x = 14; x < src.cols - 15; x++){
+            buf = 0;
+            for(int i = 0; i < 15; i++){
+                if((int)ptr[y + x + i - 7] == 0){ // 0 is black
+                    buf++;
+                }
+                if(buf == 9){
+                    ptr[y + x - 7] = 0;
+                    break;
+                }
+                else{
+                    ptr[y + x - 7] = 255;
+                }
+            }
+        }
+    }
 }
